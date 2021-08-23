b129596157307c3af91457358dc76b4d4ce4794f
Thomas Kruppi <tkruppi@gmail.com>
2021-08-23 03:55:43 +0000

    Basic SFML window creation and event handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "Welcome to TopLog!!" << std::endl;
+    sf::Window wnd(sf::VideoMode(1024, 768), "TopLog");
+
+    while (wnd.isOpen())
+    {
+        sf::Event event;
+        while(wnd.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                wnd.close();
+        }
+    }
     return 0;
 }
 
